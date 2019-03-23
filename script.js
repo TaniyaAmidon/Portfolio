28090915c2cdb430f7ed561c04820f5c83f784aa
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2019-03-23 12:29:12 +0000

    onclick function added to home page

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+const home = () => {
+  document.getElementById("main").innerHTML = document.getElementById("home_page").innerHTML;
+}
+
+const aboutMe = () => {
+  document.getElementById("main").innerHTML = document.getElementById("about_me_page").innerHTML;
+}
+
+const skills = () => {
+  document.getElementById("main").innerHTML = document.getElementById("project_page").innerHTML;
+}
+
+const contact = () => {
+  document.getElementById("main").innerHTML = document.getElementById("contact_page").innerHTML;
+}
